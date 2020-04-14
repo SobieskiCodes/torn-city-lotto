d9c9d2e1a36c12aa16c971b382c1bcef475c66c6
Justin Sobieski <jmksobieski@gmail.com>
2020-04-14 16:33:11 -0400

    re-add DM's cause lul

--- cogs/giveaway.py
+++ cogs/giveaway.py
@@ -2,7 +2,6 @@ from discord.ext import commands
 import discord
 import asyncio
 from cogs.util.checks import is_mod, is_guild_owner
-from cogs.util.errorhandling import NotAdded
 from datetime import datetime
 import random
 
@@ -41,10 +40,9 @@ class timedGiveaway(commands.Cog):
             return
 
         if user.id not in self.bot.addedids and user.id != self.bot.user.id:
-            test = await reaction.message.channel.send(f"{user.mention}, please add your torn ID using $addid <id>!")
+            await reaction.message.channel.send(f"{user.mention}, please add your torn ID using $addid <id>!",
+                                                delete_after=3)
             await reaction.message.remove_reaction(reaction.emoji, user)
-            await asyncio.sleep(3)
-            await test.delete()
             return
 
         get_the_emoji = self.bot.get_emoji(679861713293934635)
@@ -56,12 +54,11 @@ class timedGiveaway(commands.Cog):
         profile = await self.bot.torn.url.get_profiles(get_user[0])
         return profile
 
-
     async def giveaway(self, ctx, length, winners, prize):
         e = discord.Embed(
-            title=f"<:party:679861713293934635>{ctx.message.author.name} just started a giveaway!<:party:679861713293934635>",
+            title=f"{self.bot.party}{ctx.message.author.name} just started a giveaway!{self.bot.party}",
             description=f"The prize: {prize} \nGiveaway ends in {length // 60}m\nNumber of winners: {winners}\n"
-                        f"React with <:party:679861713293934635> to enter!",
+                        f"React with {self.bot.party} to enter!",
             timestamp=datetime.utcnow(),
             colour=discord.Colour(0x278d89)
         )
@@ -90,7 +87,7 @@ class timedGiveaway(commands.Cog):
                 link = await timedGiveaway.get_user_profile_link(self, person)
                 profile_links.append(f"[{person.display_name}]({link})")
             new_e = discord.Embed(
-                title=f"<:party:679861713293934635>{ctx.message.author.name}'s giveaway has ended!<:party:679861713293934635>",
+                title=f"{self.bot.party}{ctx.message.author.name}'s giveaway has ended!{self.bot.party}",
                 description=f"The prize: {prize} \nWinner: {', '.join(win.mention for win in winner)}\nNumber of entries: {len(users)}\n",
                 timestamp=datetime.utcnow(),
                 colour=discord.Colour(0x278d89)
@@ -98,7 +95,7 @@ class timedGiveaway(commands.Cog):
             e.set_footer(text="Ended")
             await the_giveaway.edit(embed=new_e)
             another_e = discord.Embed(
-                description=f"<:party:679861713293934635>{ctx.message.author.name}'s giveaway has ended! \n"
+                description=f"{self.bot.party}{ctx.message.author.name}'s giveaway has ended! \n"
                                             f"Winner: {', '.join(win.mention for win in winner)}\n"
                                             f"The prize: {prize} \n"
                                             f"[link]({the_giveaway.jump_url}) | {' | '.join(profile_links)}",
@@ -106,10 +103,12 @@ class timedGiveaway(commands.Cog):
                 colour=discord.Colour(0x278d89)
             )
             await the_giveaway.channel.send(embed=another_e)
+            author_embed = discord.Embed(description=f"Hey, the giveaway ended! [link]({the_giveaway.jump_url})")
+            await ctx.author.send(embed=author_embed)
 
         if len(users) <= 2:
             new_e = discord.Embed(
-                title=f"<:party:679861713293934635>{ctx.message.author.name}'s giveaway has ended!<:party:679861713293934635>",
+                title=f"{self.bot.party}{ctx.message.author.name}'s giveaway has ended!{self.bot.party}",
                 description=f"The prize: {prize} \nWinner: no one won because not enough people joined :(\n"
                             f"Number of entries: {len(users)}\n",
                 timestamp=datetime.utcnow(),
@@ -123,28 +122,28 @@ class timedGiveaway(commands.Cog):
     async def gstart(self, ctx, time: str = None, winners: str = None, *, prize: str = None):
         if not time or not winners or not prize:
             e = discord.Embed(colour=discord.Colour(0xbf2003),
-                                description=f"<:no:609076414469373971> {ctx.author.name}, "
+                                description=f"{self.bot.no} {ctx.author.name}, "
                                             f"format is $gstart time winners prize")
             await ctx.send(embed=e)
             return
 
         if str(time[-1:]) != 'm':
             e = discord.Embed(colour=discord.Colour(0xbf2003),
-                                description=f"<:no:609076414469373971> {ctx.author.name}, "
+                                description=f"{self.bot.no} {ctx.author.name}, "
                                             f"Time needs to be in minutes eg: 10m")
             await ctx.send(embed=e)
             return
 
         if str(winners[-1:]) != 'w':
             e = discord.Embed(colour=discord.Colour(0xbf2003),
-                                description=f"<:no:609076414469373971> {ctx.author.name}, "
+                                description=f"{self.bot.no} {ctx.author.name}, "
                                             f"please specify winners eg: 5w")
             await ctx.send(embed=e)
             return
 
         if not prize:
             e = discord.Embed(colour=discord.Colour(0xbf2003),
-                              description=f"<:no:609076414469373971> {ctx.author.name}, "
+                              description=f"{self.bot.no} {ctx.author.name}, "
                                           f"please add a prize!")
             await ctx.send(embed=e)
             return
@@ -152,13 +151,13 @@ class timedGiveaway(commands.Cog):
         if str(time[-1:]) == 'm':
             if not str(time[:-1]).isdigit():
                 e = discord.Embed(colour=discord.Colour(0xbf2003),
-                                  description=f"<:no:609076414469373971> {ctx.author.name}, "
+                                  description=f"{self.bot.no} {ctx.author.name}, "
                                               f"looks like time is not a valid digit!")
                 await ctx.send(embed=e)
                 return
             if not str(winners[:-1]).isdigit():
                 e = discord.Embed(colour=discord.Colour(0xbf2003),
-                                  description=f"<:no:609076414469373971> {ctx.author.name}, "
+                                  description=f"{self.bot.no} {ctx.author.name}, "
                                               f"looks like winners is not a valid digit!")
                 await ctx.send(embed=e)
                 return
@@ -172,12 +171,11 @@ class timedGiveaway(commands.Cog):
         """The channel you would like to use for the giveaway, $giveaway <#channel>"""
         if not chan or not ctx.message.channel_mentions:
             e = discord.Embed(colour=discord.Colour(0xbf2003),
-                              description=f"<:no:609076414469373971> {ctx.author.name}, please provide a channel to set!")
-            remove = await ctx.send(embed=e)
+                              description=f"{self.bot.no} {ctx.author.name}, please provide a channel to set!")
+            await ctx.send(embed=e, delete_after=3)
             await ctx.message.delete()
-            await asyncio.sleep(3)
-            await remove.delete()
             return
+
         if ctx.message.channel_mentions:
             if len(ctx.message.channel_mentions) == 1:
                 the_chan = ctx.message.channel_mentions[0]
@@ -186,19 +184,17 @@ class timedGiveaway(commands.Cog):
                 await self.bot.db.commit()
                 self.bot.gaveawaydict[ctx.guild.id] = the_chan.id
                 e = discord.Embed(colour=discord.Colour(0x03bd33),
-                                  description=f"<:tickYes:611582439126728716> Giveaway channel has been updated to {the_chan.name}")
-                remove = await ctx.send(embed=e)
+                                  description=f"{self.bot.yes} Giveaway channel has been updated to {the_chan.name}")
+                await ctx.send(embed=e, delete_after=3)
                 await ctx.message.delete()
-                await asyncio.sleep(3)
-                await remove.delete()
+
 
             else:
                 e = discord.Embed(colour=discord.Colour(0xbf2003),
-                                  description=f"<:no:609076414469373971> {ctx.author.name}, please provide only a single channel to set!")
-                remove = await ctx.send(embed=e)
+                                  description=f"{self.bot.no} {ctx.author.name}, please provide only a single channel to set!")
+                await ctx.send(embed=e, delete_after=3)
                 await ctx.message.delete()
-                await asyncio.sleep(3)
-                await remove.delete()
+
 
 
 def setup(bot):
